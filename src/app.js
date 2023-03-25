f26d5d6d293e80e05a40ce4e578bd658e0ffa2dc
sanchitarora802 <sanchit.arora802@gmail.com>
2023-03-25 09:11:53 +0530

    1. Express Server setup 2. NodeMon Setup

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+// giving the value or just using 5000 port
+const port = process.env.PORT || 3001;  
+
+app.get('/',(req,res) => {
+    res.send("Starting Page")
+})
+
+app.get('/students',(req,res) => {
+    res.send("hello from other sides. get")
+})
+
+app.listen(port, ()=> {
+    console.log('Connection is successful')
+})
